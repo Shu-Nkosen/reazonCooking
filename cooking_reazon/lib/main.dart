a62d55801f51b1ace1e7589bb0983d947184827a
te-uemrua <te22uemura@g.kumamoto-nct.ac.jp>
2025-08-25 16:07:35 +0900

    ジャイロお試ししたよ

--- cooking_reazon/lib/main.dart
+++ cooking_reazon/lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-
-final List<Map<String, dynamic>> listItems = [
-  {'taskName': 'Item 1', 'isDone': false},
-  {'taskName': 'Item 2', 'isDone': false},
-  {'taskName': 'Item 3', 'isDone': false},
-];
+import 'package:sensors_plus/sensors_plus.dart';
+import 'dart:async';
 
 void main() {
   runApp(MainApp());
@@ -15,131 +11,51 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Todo App', home: TodoListPage());
+    return MaterialApp(title: 'Todo App', home: MyHomePage());
   }
 }
 
-class TodoListPage extends StatefulWidget {
-  const TodoListPage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
-  State<TodoListPage> createState() => _TodoListPageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _TodoListPageState extends State<TodoListPage> {
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('タスク一覧')),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: ListView.builder(
-                itemCount: listItems.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                      leading: Checkbox(
-                        checkColor: Colors.white,
-                        value: listItems[index]["isDone"],
-                        onChanged: (bool? value) {
-                          setState(() {
-                            listItems[index]["isDone"] = value!;
-                          });
-                        },
-                      ),
-                      title: listItems[index]["isDone"]
-                          ? Text(
-                              listItems[index]["taskName"],
-                              style: const TextStyle(
-                                decoration: TextDecoration.lineThrough,
-                              ),
-                            )
-                          : Text(listItems[index]["taskName"],
-                            ),
-                      trailing: Icon(Icons.delete),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            final newText = await Navigator.of(context).push<String>(
-              MaterialPageRoute(builder: (context) => const NextPage()),
-            );
-
-            // 戻り値がnullでない（何かしらの値が入力された）場合、リストに追加
-            if (newText != "" && newText != null) {
-              setState(() {
-                listItems.add({"taskName": newText, "isDone": false});
-              });
-            }
-            // 戻ってきた値をDebug Logに表示
-            // debugPrint(newText);
-          },
-
-          tooltip: 'NextPage',
-          child: const Icon(Icons.add),
-        ),
-      ),
-    );
-  }
-}
-
-// 次のページ (NextPage)
-class NextPage extends StatefulWidget {
-  const NextPage({super.key});
+class _MyHomePageState extends State<MyHomePage> {
+  String _userAccelerometerValues = "";
+  String _gyroscopeValues = "";
 
   @override
-  State<NextPage> createState() => _NextPageState();
-}
-
-class _NextPageState extends State<NextPage> {
-  String taskContent = '';
-
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('次のページ')),
+      appBar: AppBar(title: Text("Flutter Demo Home Page")),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Center(
-            child: Container(
-              width: 300,
-              child: TextField(
-                onChanged: (value) {
-                  setState(() {
-                    taskContent = value;
-                  });
-                },
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'タスクの内容',
-                ),
-              ),
-            ),
+          Text(
+            _userAccelerometerValues,
+            style: Theme.of(context).textTheme.titleLarge,
           ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop(taskContent);
-              },
-              child: const Text('タスクを追加'),
-            ),
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('キャンセル'),
-            ),
-          ),
+          Text(_gyroscopeValues, style: Theme.of(context).textTheme.titleLarge),
         ],
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    userAccelerometerEvents.listen((UserAccelerometerEvent event) {
+      setState(() {
+        _userAccelerometerValues =
+            "加速度センサー\n${event.x}\n${event.y}\n${event.z}";
+      });
+    });
+    gyroscopeEvents.listen((GyroscopeEvent event) {
+      setState(() {
+        _gyroscopeValues = "ジャイロセンサー\n${event.x}\n${event.y}\n${event.z}";
+      });
+    });
   }
 }
